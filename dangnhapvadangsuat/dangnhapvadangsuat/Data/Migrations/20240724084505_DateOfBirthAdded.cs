1ffc104a781659f72770500de77e10ab88cdb9b1
TrinhThanhDat <datt40006@gmail.com>
2024-07-25 09:00:17 +0700

    Add project files.

---
+++ dangnhapvadangsuat/dangnhapvadangsuat/Data/Migrations/20240724084505_DateOfBirthAdded.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dangnhapvadangsuat.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateOfBirthAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfbirth",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfbirth",
+                table: "AspNetUsers");
+        }
+    }
+}
